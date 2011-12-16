a4f3c37bd96fc0a4ba87c1a3f6adc1de4996aa91
Boaz Sender <boaz@bocoup.com>
2011-12-16 11:47:11 -0500

    guy works

--- js/entity.js
+++ js/entity.js
@@ -20,8 +20,56 @@ document.addEventListener("DOMContentLoaded", function() {
       x: 0,
       y: 14,
       width: 20,
-      height: .5,
+      height: .2,
       type: 'static'
     });
+
+    player.onKeydown(function( e ){
+
+      var key = keyDecode( e );
+      var movementForce = 50;
+      
+      if( key === 'right' ) {
+        this.setForce( 'movement', movementForce, 90);
+      }
+
+      if( key === 'left' ) {
+        this.setForce( 'movement', movementForce, 270);
+      }
+
+      if( key === 'down' ) {
+        this.setForce( 'movement', movementForce, 180);
+      }
+
+      if( this.contact ) {
+        
+        if( key === 'up' ) {
+          this.applyImpulse( 10, 0);
+        }
+
+        if( key === 'space' ) {
+          this.applyImpulse( 10, 0);
+        }
+
+      }
+      
+    });
+    
+    player.onKeyup(function( e ){
+      this.clearForce( 'movement' );
+    });
+    
+    player.onStartContact(function( e ){
+      console.log('start')
+      this.contact = true;
+      
+    });
+    
+    player.onFinishContact(function( e ){
+      console.log('stop')
+      this.contact = false;
+    });
+    
+    
 
 }, false);
