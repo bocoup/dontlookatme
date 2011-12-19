b0297291b44b48d065df46edf1a7509516b36c71
Boaz Sender <boaz@bocoup.com>
2011-12-18 21:57:35 -0500

    implemented death, and better player physics

--- js/dlam.player.js
+++ js/dlam.player.js
@@ -1,77 +1,71 @@
 (function( global, dlam, boxbox ) {
-  dlam.player = function(){
+ dlam.player = function(){
 
-    var player = dlam.player = dlam.world.createEntity({
-        name: 'player',
-        x: 100,
-        y: 80,
-        radius: 1,
-        fixedRotation: true,
-        friction: 0,
-        restitution: 0,
-        color: 'blue',
-        shape: 'circle',
-        density: 0
-    });
+   var player = dlam.player = dlam.world.createEntity({
+       name: 'player',
+       x: 100,
+       y: 80,
+       radius: 1,
+       fixedRotation: true,
+       friction: 2,
+       restitution: 0,
+       color: 'blue',
+       shape: 'circle',
+       density: 0
+   });
 
-    player.health = 100;
-    player.points = 0;
+   player.health = 100;
+   player.points = 0;
 
-    player.onKeydown(function( e ){
+   player.onKeydown(function( e ){
 
-      var key = global.keyDecode( e );
-      var movementForce = 160;
-      var impulseForce = 40;
-      var lastJump = Date.now();
+     var key = global.keyDecode( e );
+     var movementForce = 300;
+     var impulseForce = 100;
+     var lastJump = Date.now();
 
-      if( key === 'right' ) {
-        this.setForce( 'movement', movementForce, 90);
-      }
+     if( key === 'right' ) {
+       this.setForce( 'movement', movementForce, 1 * movementForce, 0);
+     }
 
-      if( key === 'left' ) {
-        this.setForce( 'movement', movementForce, 270);
-      }
+     if( key === 'left' ) {
+       this.setForce( 'movement', movementForce, -1 * movementForce, 0);
+     }
 
-      if( key === 'down' ) {
-        this.setForce( 'movement', movementForce, 180);
-      }
+     if( this.contact && this.jumps < 2 ) {
 
-      if( this.contact && this.jumps < 2 ) {
+       if( key === 'up' || key === 'space' ) {
+         this.jumps++
+         this.applyImpulse( impulseForce, 0);
+       }
+     }
 
-        this.applyImpulse( impulseForce, 0);
-        if( key === 'up' || key === 'space' ) {
-          this.jumps++
-          this.applyImpulse( impulseForce, 0);
-        }
+   });
 
-      }
+   player.onKeyup(function( e ){
+     player.clearForce( 'movement' );
+   });
 
-    });
+   player.onStartContact(function( e ){
+     this.contact = true;
+     this.jumps = 0;
 
-    player.onKeyup(function( e ){
-      player.clearForce( 'movement' );
-    });
+   });
 
-    player.onStartContact(function( e ){
-      this.contact = true;
-      this.jumps = 0;
+   player.onFinishContact(function( e ){
+     this.contact = false;
+   });
 
-    });
+   player.onImpact(function( entity, force, friction ){
+     if( force > 10 && entity._ops.type !== 'static' && entity.name !== 'obstacle'){
+       player.health-50
+       console.log(player.health)
+       if ( player.health > 0) {
 
-    player.onFinishContact(function( e ){
-      this.contact = false;
-    });
+         player.destroy()
+       }
+     }
+   })
 
-    player.onImpact(function( entity, force, friction ){
-      if( force > 81110 && entity._ops.type !== 'static' && entity.name !== 'obstacle'){
-        player.health-50
-        console.log(player.health)
-        if ( player.health > 0) {
-
-          player.destroy()
-        }
-      }
-    })
-
-  }
+ }
 }( this, this.dlam, this.boxbox ));
